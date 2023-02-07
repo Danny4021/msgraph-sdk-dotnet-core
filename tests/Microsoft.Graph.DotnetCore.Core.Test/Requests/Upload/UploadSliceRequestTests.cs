3c81211dfd94faa1cf65f966d2a2cf8abb33116f
Andrew Omondi <andrew.omondi@microsoft.com>
2023-02-07 22:18:41 +0300

    test upload

--- tests/Microsoft.Graph.DotnetCore.Core.Test/Requests/Upload/UploadSliceRequestTests.cs
+++ tests/Microsoft.Graph.DotnetCore.Core.Test/Requests/Upload/UploadSliceRequestTests.cs
@@ -5,11 +5,14 @@
 namespace Microsoft.Graph.DotnetCore.Core.Test.Requests
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
+    using System.Net.Http.Headers;
     using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Graph.DotnetCore.Core.Test.Mocks;
     using Microsoft.Graph.DotnetCore.Core.Test.TestModels.ServiceModels;
@@ -20,44 +23,89 @@ namespace Microsoft.Graph.DotnetCore.Core.Test.Requests
         [Fact]
         public async Task PutAsyncReturnsExpectedUploadSessionAsync()
         {
-            using (HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.OK))
-            using (TestHttpMessageHandler testHttpMessageHandler = new TestHttpMessageHandler())
-            {
+            
                 /* Arrange */
                 // 1. create a mock response
                 string requestUrl = "https://localhost/";
-                string responseJSON = @"{
+
+
+            // 3. Create a batch request object to be tested
+                var handlers = GraphClientFactory.CreateDefaultHandlers(null);
+                handlers.Add(new UploadHttpMessageHandler());
+                var httpClient = GraphClientFactory.Create(handlers);
+                httpClient.Timeout = TimeSpan.FromSeconds(5);
+                var baseClient = new BaseClient(requestUrl, httpClient);
+
+            bool firstAttempt = true;
+                try
+                {
+                    UploadSliceRequest<TestDriveItem> uploadSliceRequest = new UploadSliceRequest<TestDriveItem>(requestUrl, baseClient, 0, 200, 1000);
+                    Stream stream = new MemoryStream(new byte[300]);
+
+                    /* Act */
+                    var uploadResult = await uploadSliceRequest.PutAsync(stream);
+                }
+                catch (ServiceException exception)
+                {
+                    if (exception.IsMatch("generalException") || exception.IsMatch("timeout"))
+                    {
+                        if (firstAttempt)
+                        {
+                            firstAttempt = false;
+                            //exceptionTrackingList.Add(exception);
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    else if (exception.IsMatch("invalidRange"))
+                    {
+                        // Succeeded previously, but nothing to return right now
+                         new UploadResult<string>();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            
+
+                //var uploadSession = uploadResult.UploadSession;
+
+                /* Assert */
+                //Assert.False(uploadResult.UploadSucceeded);
+
+        }
+    }
+    public class UploadHttpMessageHandler : DelegatingHandler
+    {
+        public UploadHttpMessageHandler()
+        {
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var httpRequestMessage = new HttpRequestMessage() { RequestUri = new Uri("https://api.rapidmock.com/mocks/89mEw") };
+            httpRequestMessage.Headers.TryAddWithoutValidation("x-rapidmock-delay", "9000");
+
+            var testResponse =await base.SendAsync(httpRequestMessage, cancellationToken);
+            //await Task.Delay(32000);
+            HttpResponseMessage responseMessage = new HttpResponseMessage();
+
+            string responseJSON = @"{
                   ""expirationDateTime"": ""2015 - 01 - 29T09: 21:55.523Z"",
                   ""nextExpectedRanges"": [
                   ""12345-55232"",
                   ""77829-99375""
                   ]
                 }";
-                HttpContent content = new StringContent(responseJSON, Encoding.UTF8, CoreConstants.MimeTypeNames.Application.Json);
-                responseMessage.Content = content;
+            HttpContent content = new StringContent(responseJSON, Encoding.UTF8, CoreConstants.MimeTypeNames.Application.Json);
+            responseMessage.Content = content;
 
-                // 2. Map the response
-                testHttpMessageHandler.AddResponseMapping(requestUrl, responseMessage);
-
-                // 3. Create a batch request object to be tested
-                MockCustomHttpProvider customHttpProvider = new MockCustomHttpProvider(testHttpMessageHandler);
-                BaseClient client = new BaseClient(requestUrl, authenticationProvider.Object, customHttpProvider);
-                UploadSliceRequest<TestDriveItem> uploadSliceRequest = new UploadSliceRequest<TestDriveItem>(requestUrl, client, 0, 200, 1000);
-                Stream stream = new MemoryStream(new byte[300]);
-
-                /* Act */
-                var uploadResult = await uploadSliceRequest.PutAsync(stream);
-                var uploadSession = uploadResult.UploadSession;
-
-                /* Assert */
-                Assert.False(uploadResult.UploadSucceeded);
-                Assert.NotNull(uploadSession);
-                Assert.Null(uploadSession.UploadUrl);
-                Assert.Equal(DateTimeOffset.Parse("2015 - 01 - 29T09: 21:55.523Z"), uploadSession.ExpirationDateTime);
-                Assert.Equal("12345-55232", uploadSession.NextExpectedRanges.First());
-                Assert.Equal("77829-99375", uploadSession.NextExpectedRanges.Last());
-                Assert.Equal(2, uploadSession.NextExpectedRanges.Count());
-            }
+            return responseMessage;
         }
+
     }
 }
